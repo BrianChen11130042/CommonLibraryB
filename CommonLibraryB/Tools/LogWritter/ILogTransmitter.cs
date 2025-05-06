b75ae319ee9a90df6be579ae3f17a4fd09b43a12
Brian.Chen <Brian.Chen@A11130042PC>
2025-05-06 13:41:42 +0800

    新增底層

---
+++ CommonLibraryB/Tools/LogWritter/ILogTransmitter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibraryB.Tools.LogWritter
+{
+    public interface ILogTransmissionObservable
+    {
+        void AddLogTransmissionObserver(ILogTransmissionObserver o);
+    }
+
+    public interface ILogTransmissionObserver
+    {
+        Task NotifyLog(EStatus status, string msg);
+    }
+}
