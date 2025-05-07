68096b795b53457caa4ba632b3658c876dd0da43
Brian.Chen <Brian.Chen@A11130042PC>
2025-05-07 17:46:38 +0800

    新增AmrConfig

--- CommonLibraryB/Library/Amr/Config/AmrConfig.cs
+++ CommonLibraryB/Library/Amr/Config/AmrConfig.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommonLibraryB.Manager.ModbusTcp.Master;
 
 namespace CommonLibraryB.Library.Amr.Config
 {
     public class AmrConfig
     {
+        public string AmrName { get; set; }
 
+        public string AmrId { get; set; }
+
+        public EModbusTcpMaster tcpMaster { get; set; }
     }
 }
