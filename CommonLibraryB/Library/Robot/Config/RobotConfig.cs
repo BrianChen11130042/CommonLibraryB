b04f343785aa9438c86beb9cb1a11f302e1b7048
Brian.Chen <Brian.Chen@A11130042PC>
2025-05-16 10:26:32 +0800

    新增robot config

---
+++ CommonLibraryB/Library/Robot/Config/RobotConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibraryB.Library.Robot.Config
+{
+    internal class RobotConfig
+    {
+    }
+}
