ec1f085661cd68e0c5ac8a026b22eb4b0b53e82d
Brian.Chen <Brian.Chen@A11130042PC>
2025-08-17 13:11:54 +0800

    modbus tcp disconnect 新增dispose

--- CommonLibraryB/Manager/ModbusTcp/Master/ModbusTcpMasterConfig.cs
+++ CommonLibraryB/Manager/ModbusTcp/Master/ModbusTcpMasterConfig.cs
@@ -79,6 +79,7 @@ namespace CommonLibraryB.Manager.ModbusTcp.Master
                 if (tcpClient.Connected)
                 {
                     tcpClient.Close();
+                    tcpClient.Dispose();
                 }
 
                 return true;
