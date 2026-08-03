d1ef60e825b16452a09a4e7b75ee7e56f6449afe
Brian.Chen <Brian.Chen@A11130042PC>
2026-08-03 15:45:15 +0800

    新增web api client manager 底層

---
+++ CommonLibraryB/Manager/WebApiClient/WebApiClientConfig.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CommonLibraryB.Manager.WebApiClient
+{
+    public enum EHttp
+    {
+        http,
+        https
+    }
+
+    public class WebApiClientConfig
+    {
+        [Required]
+        public EHttp Http { get; set; } = EHttp.http;
+
+        [Required]
+        [RegularExpression(@"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)")]
+        public string Ip { get; set; } = "127.0.0.1";
+
+        [Required]
+        [Range(0, 65535)]
+        public int Port { get; set; } = 1443;
+
+        public string device { get; set; }
+
+        public bool Enable { get; set; } = false;
+
+        [JsonIgnore]
+        public HttpClient httpClient { get; set; }
+
+        [JsonIgnore]
+        public string baseUrl => $"{Http}://{Ip}:{Port}/";
+
+        public void Init()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public bool Connect(out string msg)
+        {
+            msg = string.Empty;
+
+            if (!Enable)
+                return true;
+            try
+            {
+
+                httpClient.BaseAddress = new Uri(baseUrl);
+                httpClient.Timeout = TimeSpan.FromSeconds(30);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+                return false;
+            }
+        }
+
+        public bool Disconnect()
+        {
+            if (!Enable)
+                return true;
+
+            try
+            {
+                httpClient?.Dispose();
+                httpClient = null;
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CommonLibraryB/Manager/WebApiClient/WebApiClientPack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.JSInterop.Implementation;
+
+namespace CommonLibraryB.Manager.WebApiClient
+{
+
+    public class WebApiClientPack
+    {
+        public HttpClient httpClient { get; set; }
+
+        public string path { get; set; }
+
+        public async Task<TResponse> PostAsync<TRequest, TResponse>(string path, TRequest request)
+        {
+            using (var response = await httpClient.PostAsJsonAsync(path, request))
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+
+                if(!response.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException($"HTTP {(int)response.StatusCode}: {jsonString}");
+                }
+
+                var data = JsonSerializer.Deserialize<TResponse>(jsonString);
+
+                if(data == null)
+                {
+                    throw new InvalidOperationException("response deserialize failed");
+                }
+
+                return data;
+            }
+        }
+    }
+}
